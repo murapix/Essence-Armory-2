5ec32462e9ce68bce7980e5dbd9ef45291e90756
murapix <smartseth@hotmail.com>
2017-01-14 14:05:40 -0800

    Redstone Arrow finally works

--- src/main/java/com/teamwizardry/inhumanresources/common/items/ItemRedstoneArrow.java
+++ src/main/java/com/teamwizardry/inhumanresources/common/items/ItemRedstoneArrow.java
@@ -1,7 +1,13 @@
 package com.teamwizardry.inhumanresources.common.items;
 
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Enchantments;
+import net.minecraft.item.ItemArrow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import com.teamwizardry.inhumanresources.common.entity.EntityRedstoneArrow;
 import com.teamwizardry.inhumanresources.common.utils.Util;
@@ -15,8 +21,16 @@ public class ItemRedstoneArrow extends ItemModArrow
 		setCreativeTab(Util.tabEssence);
 	}
 
-	public EntityArrow createArrow(World world, EntityLivingBase shooter)
+	@Override
+	public EntityArrow generateArrowEntity(World world, ItemStack arrow, Vec3d dir, EntityLivingBase shooter)
 	{
 		return new EntityRedstoneArrow(world, shooter);
+	}
+
+	@Override
+	public boolean isInfinite(ItemStack arrow, ItemStack bow, EntityPlayer player)
+	{
+		int enchant = EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, bow);
+		return enchant > 0 && this instanceof ItemArrow;
 	}
 }
